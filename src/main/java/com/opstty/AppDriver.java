98f5fed5631f00a5adce74a2761f8739c61bb804
salahbgm <116580792+salahbgm@users.noreply.github.com>
2024-07-15 09:22:29 +0200

    update AppDriver.java

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,6 @@
 package com.opstty;
 
-import com.opstty.job.WordCount;
+import com.opstty.job.*;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,6 +11,22 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("districts", DistrictsJob.class,
+                    "A map/reduce program that lists the districts containing trees.");
+            programDriver.addClass("species", SpeciesJob.class,
+                    "A map/reduce program that lists all existing species.");
+            programDriver.addClass("treesbyspecies", TreesBySpeciesJob.class,
+                    "A map/reduce program that counts the number of trees of each species.");
+            programDriver.addClass("maxheight", MaxHeightJob.class,
+                    "A map/reduce program that finds the maximum height of each species.");
+            programDriver.addClass("sortheight", SortHeightJob.class,
+                    "A map/reduce program that sorts the heights of trees from smallest to largest.");
+            programDriver.addClass("oldesttree", OldestTreeJob.class,
+                    "A map/reduce program that finds the district containing the oldest tree.");
+            programDriver.addClass("maxtreedistrict", TreeCountJob.class,
+                    "A map/reduce program that finds the district containing the most trees (Phase 1).");
+            programDriver.addClass("maxtreedistrict2", MaxTreeDistrictJob.class,
+                    "A map/reduce program that finds the district containing the most trees (Phase 2).");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
